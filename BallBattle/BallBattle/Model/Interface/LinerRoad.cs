4091733c26f29c1e32e2cd6c028198d57a415f8d
a1237981379 <554286238@qq.com>
2016-07-01 14:41:50 +0800

    补

---
+++ BallBattle/BallBattle/Model/Interface/LinerRoad.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace BallBattle.Model.Interface
+{
+    class LinerRoad : RoadInterface
+    {
+
+
+        private int direction = 1;//
+        
+        public LinerRoad(BaseBall ball)
+            : base(ball)
+        {
+
+        }
+
+        public override Vector2 getDirection()
+        {
+
+            return new Vector2(0, direction*1);
+        }
+
+        public override Vector2 getPosition()
+        {
+            Vector2 v = new Vector2(new Random().Next(WallManager.wallRect.Width), ball.getRect().Height * ball.scale);
+            Console.WriteLine(v);
+            return v;
+        }
+
+        public override bool isOutDo()
+        {
+            times_out++;
+            if (times_out < 6)
+            {
+                direction *= -1;
+                return false;
+            }
+            else
+            {
+
+                return true;
+            }
+
+            return base.isOutDo();
+        }
+
+    }
+}
